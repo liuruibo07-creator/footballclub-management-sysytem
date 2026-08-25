ae39490332d93ecc3c94d415d0b956b36b35963e
lrb <2772156232@qq.com>
2026-08-25 13:52:27 +0800

    项目后端架构搭建完成

---
+++ fc-admin/src/main/java/com/fc/FCApplication.java
@@ -0,0 +1,21 @@
+package com.fc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ * 
+ * @author ruoyi
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class FCApplication
+{
+    public static void main(String[] args)
+    {
+        // System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(FCApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  服务启动成功   ლ(´ڡ`ლ)ﾞ  \n");
+    }
+}
